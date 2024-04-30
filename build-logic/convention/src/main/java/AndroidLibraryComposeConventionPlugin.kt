173a74782ee2d54d073aa88a869b71a3a398785a
Michal Cadrik <cadrikmdev@gmail.com>
2024-04-30 16:20:30 +0200

    added android room plugin

--- build-logic/convention/src/main/java/AndroidLibraryComposeConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidLibraryComposeConventionPlugin.kt
@@ -1,4 +1,3 @@
-import com.android.build.api.dsl.ApplicationExtension
 import com.android.build.api.dsl.LibraryExtension
 import com.cadrikmdev.convention.configureAndroidCompose
 import org.gradle.api.Plugin
