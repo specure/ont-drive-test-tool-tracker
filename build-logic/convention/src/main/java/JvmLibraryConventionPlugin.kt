cec9bbb8260db6d4ace56ec2a2fcfc027c5bf88e
Michal Cadrik <cadrikmdev@gmail.com>
2024-04-30 16:56:33 +0200

    cleaning

--- build-logic/convention/src/main/java/JvmLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/java/JvmLibraryConventionPlugin.kt
@@ -1,13 +1,6 @@
-import com.android.build.api.dsl.LibraryExtension
-import com.cadrikmdev.convention.ExtensionType
-import com.cadrikmdev.convention.configureBuildTypes
-import com.cadrikmdev.convention.configureKotlinAndroid
 import com.cadrikmdev.convention.configureKotlinJvm
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.kotlin
 
 class JvmLibraryConventionPlugin: Plugin<Project> {
     override fun apply(target: Project) {
