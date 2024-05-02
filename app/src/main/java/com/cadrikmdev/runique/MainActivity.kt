a2cc3ddd72b5807473c2b4809fb04a0e5cb5015e
Michal Cadrik <cadrikmdev@gmail.com>
2024-05-02 13:48:26 +0200

    Created theme

--- app/src/main/java/com/cadrikmdev/runique/MainActivity.kt
+++ app/src/main/java/com/cadrikmdev/runique/MainActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.cadrikmdev.runique.ui.theme.RuniqueTheme
+import com.cadrikmdev.core.presentation.designsystem.AnalyticsIcon
+import com.cadrikmdev.core.presentation.designsystem.RuniqueTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Icon(imageVector = AnalyticsIcon, contentDescription = null)
                 }
             }
         }
