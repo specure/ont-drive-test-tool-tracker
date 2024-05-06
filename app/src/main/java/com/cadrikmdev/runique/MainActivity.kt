b29ddf109fbe9a661eba76b7c399544894e2d64b
Michal Cadrik <cadrikmdev@gmail.com>
2024-05-06 13:43:09 +0200

    created navigation root to intro screen and to register screen

--- app/src/main/java/com/cadrikmdev/runique/MainActivity.kt
+++ app/src/main/java/com/cadrikmdev/runique/MainActivity.kt
@@ -4,14 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.cadrikmdev.core.presentation.designsystem.AnalyticsIcon
+import androidx.navigation.compose.rememberNavController
 import com.cadrikmdev.core.presentation.designsystem.RuniqueTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,25 +20,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Icon(imageVector = AnalyticsIcon, contentDescription = null)
+                    val navController = rememberNavController()
+                    NavigationRoot(navController = navController)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RuniqueTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/cadrikmdev/runique/NavigationRoot.kt
@@ -0,0 +1,53 @@
+package com.cadrikmdev.runique
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.cadrikmdev.presentation.intro.IntroScreenRoot
+import com.cadrikmdev.presentation.registration.RegisterScreenRoot
+
+@Composable
+fun NavigationRoot(
+    navController: NavHostController
+) {
+    NavHost(
+        navController = navController,
+        startDestination = "auth"
+    ) {
+        authGraph(navController = navController)
+    }
+}
+
+private fun NavGraphBuilder.authGraph(navController: NavHostController) {
+    navigation(
+        startDestination = "intro",
+        route = "auth"
+    ) {
+        composable(route = "intro") {
+            IntroScreenRoot(
+                onSignInClick = {
+                    navController.navigate("login")
+                },
+                onSignUpClick = {
+                    navController.navigate("register")
+                },
+            )
+        }
+        composable(route = "register") {
+            RegisterScreenRoot(
+                onSingInClick = {
+                    navController.navigate("login") {
+                        popUpTo("register") {
+                            inclusive = true
+                            saveState = true
+                        }
+                        restoreState = true
+                    }
+                },
+                onSuccessfulRegistration = { navController.navigate("login") })
+        }
+    }
+}
