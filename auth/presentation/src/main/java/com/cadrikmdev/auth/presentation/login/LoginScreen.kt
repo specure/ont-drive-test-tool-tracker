8e80be3150cd5dae1809f14593fa34d3c63df39b
Michal Cadrik <cadrikmdev@gmail.com>
2024-05-09 11:56:23 +0200

    solving logging in screen text missing for portrait mode - landscape mode is missing the text

--- auth/presentation/src/main/java/com/cadrikmdev/auth/presentation/login/LoginScreen.kt
+++ auth/presentation/src/main/java/com/cadrikmdev/auth/presentation/login/LoginScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.ClickableText
@@ -92,8 +93,8 @@ private fun LoginScreen(
     GradientBackground {
         Column(
             modifier = Modifier
-                .verticalScroll(rememberScrollState())
                 .fillMaxSize()
+                .verticalScroll(rememberScrollState())
                 .padding(horizontal = 16.dp)
                 .padding(vertical = 32.dp)
                 .padding(top = 16.dp)
@@ -167,7 +168,8 @@ private fun LoginScreen(
             Box(
                 modifier = Modifier
                     .align(Alignment.CenterHorizontally)
-                    .weight(1f),
+                    .weight(1f)
+                    .imePadding(),
                 contentAlignment = Alignment.BottomCenter
             ) {
                 ClickableText(
