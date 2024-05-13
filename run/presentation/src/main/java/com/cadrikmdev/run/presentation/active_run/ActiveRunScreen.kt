c6b8b54e73cce74debb4216747c253a1bd3622af
Michal Cadrik <cadrikmdev@gmail.com>
2024-05-13 10:36:19 +0200

    Permission asking logic

--- run/presentation/src/main/java/com/cadrikmdev/run/presentation/active_run/ActiveRunScreen.kt
+++ run/presentation/src/main/java/com/cadrikmdev/run/presentation/active_run/ActiveRunScreen.kt
@@ -2,6 +2,13 @@
 
 package com.cadrikmdev.run.presentation.active_run
 
+import android.Manifest
+import android.content.Context
+import android.os.Build
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,18 +17,26 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cadrikmdev.core.presentation.designsystem.RuniqueTheme
 import com.cadrikmdev.core.presentation.designsystem.StartIcon
 import com.cadrikmdev.core.presentation.designsystem.StopIcon
+import com.cadrikmdev.core.presentation.designsystem.components.RuniqueDialog
 import com.cadrikmdev.core.presentation.designsystem.components.RuniqueFloatingActionButton
+import com.cadrikmdev.core.presentation.designsystem.components.RuniqueOutlinedActionButton
 import com.cadrikmdev.core.presentation.designsystem.components.RuniqueScaffold
 import com.cadrikmdev.core.presentation.designsystem.components.RuniqueToolbar
 import com.cadrikmdev.run.presentation.R
 import com.cadrikmdev.run.presentation.active_run.components.RunDataCard
+import com.cadrikmdev.run.presentation.util.hasLocationPermission
+import com.cadrikmdev.run.presentation.util.hasNotificationPermission
+import com.cadrikmdev.run.presentation.util.shouldShowLocationPermissionRationale
+import com.cadrikmdev.run.presentation.util.shouldShowNotificationPermissionRationale
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -39,6 +54,59 @@ private fun ActiveRunScreen(
     state: ActiveRunState,
     onAction: (ActiveRunAction) -> Unit
 ) {
+    val context = LocalContext.current
+    val permissionLauncher =
+        rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.RequestMultiplePermissions()
+        ) { perms ->
+            val hasCourseLocationPermission =
+                perms[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+            val hasFineLocationPermission = perms[Manifest.permission.ACCESS_FINE_LOCATION] == true
+            val hasNotificationPermission = if (Build.VERSION.SDK_INT >= 33) {
+                perms[Manifest.permission.ACCESS_FINE_LOCATION] == true
+            } else true
+            val activity = context as ComponentActivity
+            val showLocationRationale = activity.shouldShowLocationPermissionRationale()
+            val showNotificationRationale = activity.shouldShowNotificationPermissionRationale()
+
+            onAction(
+                ActiveRunAction.SubmitLocationPermissionInfo(
+                    acceptedLocationPermission = hasCourseLocationPermission && hasFineLocationPermission,
+                    showLocationRationale = showLocationRationale
+                )
+            )
+
+            onAction(
+                ActiveRunAction.SubmitNotificationPermissionInfo(
+                    acceptedNotificationPermission = hasNotificationPermission,
+                    showNotificationRationale = showNotificationRationale
+                )
+            )
+        }
+    LaunchedEffect(key1 = true) {
+        val activity = context as ComponentActivity
+        val showLocationRationale = activity.shouldShowLocationPermissionRationale()
+        val showNotificationRationale = activity.shouldShowNotificationPermissionRationale()
+
+        onAction(
+            ActiveRunAction.SubmitLocationPermissionInfo(
+                acceptedLocationPermission = context.hasLocationPermission(),
+                showLocationRationale = showLocationRationale
+            )
+        )
+
+        onAction(
+            ActiveRunAction.SubmitNotificationPermissionInfo(
+                acceptedNotificationPermission = context.hasNotificationPermission(),
+                showNotificationRationale = showNotificationRationale
+            )
+        )
+        if (!showLocationRationale && !showNotificationRationale) {
+            permissionLauncher.requestRuniquePermissions(context)
+
+        }
+    }
+
     RuniqueScaffold(
         withGradient = false,
         topAppBar = {
@@ -83,7 +151,63 @@ private fun ActiveRunScreen(
                     .fillMaxWidth()
             )
         }
+    }
 
+    if (state.showLocationRationale || state.showNotificationRationale) {
+        RuniqueDialog(
+            title = stringResource(id = R.string.permission_required),
+            onDismiss = { /* Normal dismissing not allowed for permission. */ },
+            description = when {
+                state.showLocationRationale && state.showNotificationRationale -> {
+                    stringResource(id = R.string.location_notification_rationale)
+                }
+
+                state.showLocationRationale -> {
+                    stringResource(id = R.string.location_rationale)
+                }
+
+                else -> {
+                    stringResource(id = R.string.location_rationale)
+                }
+            },
+            primaryButton = {
+                RuniqueOutlinedActionButton(
+                    text = stringResource(id = R.string.ok),
+                    isLoading = false,
+                    onClick = {
+                        onAction(ActiveRunAction.DismissRationaleDialog)
+                        permissionLauncher.requestRuniquePermissions(context)
+                    }
+                )
+            }) {
+
+        }
+    }
+
+}
+
+private fun ActivityResultLauncher<Array<String>>.requestRuniquePermissions(
+    context: Context
+) {
+    val hasLocationPermission = context.hasLocationPermission()
+    val hasNotificationPermission = context.hasNotificationPermission()
+
+    val locationPermission = arrayOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+
+    val notificationPermission = if (Build.VERSION.SDK_INT >= 33) {
+        arrayOf(Manifest.permission.POST_NOTIFICATIONS)
+    } else arrayOf()
+
+    when {
+        !hasLocationPermission && !hasNotificationPermission -> {
+            launch(locationPermission + notificationPermission)
+        }
+
+        !hasLocationPermission -> launch(locationPermission)
+        !hasNotificationPermission -> launch(notificationPermission)
     }
 }
 
---
+++ run/presentation/src/main/java/com/cadrikmdev/run/presentation/util/PermissionUtil.kt
@@ -0,0 +1,34 @@
+package com.cadrikmdev.run.presentation.util
+
+import android.Manifest
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.activity.ComponentActivity
+import androidx.core.content.ContextCompat
+
+fun ComponentActivity.shouldShowLocationPermissionRationale(): Boolean {
+    return shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)
+}
+
+fun ComponentActivity.shouldShowNotificationPermissionRationale(): Boolean {
+    return Build.VERSION.SDK_INT >= 33 && shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)
+}
+
+fun Context.hasLocationPermission(): Boolean {
+    return hasPermission(ACCESS_FINE_LOCATION)
+}
+
+fun Context.hasNotificationPermission(): Boolean {
+    return if (Build.VERSION.SDK_INT >= 33) {
+        hasPermission(Manifest.permission.POST_NOTIFICATIONS)
+    } else true
+}
+
+private fun Context.hasPermission(permission: String): Boolean {
+    return ContextCompat.checkSelfPermission(
+        this,
+        permission
+    ) == PackageManager.PERMISSION_GRANTED
+}
