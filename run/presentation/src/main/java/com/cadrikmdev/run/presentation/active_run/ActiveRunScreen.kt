834a2e52d7ad7e54e670d8151a25c7ec7274fe89
Michal Cadrik <cadrikmdev@gmail.com>
2024-05-14 09:19:49 +0200

    Pausing and finishing run

--- run/presentation/src/main/java/com/cadrikmdev/run/presentation/active_run/ActiveRunScreen.kt
+++ run/presentation/src/main/java/com/cadrikmdev/run/presentation/active_run/ActiveRunScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.unit.dp
 import com.cadrikmdev.core.presentation.designsystem.RuniqueTheme
 import com.cadrikmdev.core.presentation.designsystem.StartIcon
 import com.cadrikmdev.core.presentation.designsystem.StopIcon
+import com.cadrikmdev.core.presentation.designsystem.components.RuniqueActionButton
 import com.cadrikmdev.core.presentation.designsystem.components.RuniqueDialog
 import com.cadrikmdev.core.presentation.designsystem.components.RuniqueFloatingActionButton
 import com.cadrikmdev.core.presentation.designsystem.components.RuniqueOutlinedActionButton
@@ -160,6 +161,37 @@ private fun ActiveRunScreen(
                     .fillMaxWidth()
             )
         }
+    }
+
+    if (!state.shouldTrack && state.hasStartedRunning) {
+        RuniqueDialog(title = stringResource(
+            id = R.string.running_is_paused
+        ),
+            onDismiss = {
+                onAction(ActiveRunAction.OnResumeRunClick)
+            },
+            description = stringResource(id = R.string.resume_or_finish_run),
+            primaryButton = {
+                RuniqueActionButton(
+                    text = stringResource(id = R.string.resume),
+                    isLoading = false,
+                    onClick = {
+                        onAction(ActiveRunAction.OnResumeRunClick)
+                    },
+                    modifier = Modifier.weight(1f)
+                )
+            },
+            secondaryButton = {
+                RuniqueOutlinedActionButton(
+                    text = stringResource(id = R.string.finish),
+                    isLoading = state.isSavingRun,
+                    onClick = {
+                        onAction(ActiveRunAction.OnFinishRunClick)
+                    },
+                    modifier = Modifier.weight(1f)
+                )
+            }
+        )
     }
 
     if (state.showLocationRationale || state.showNotificationRationale) {
